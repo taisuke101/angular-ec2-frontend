9c72f90131ae58e17eeec9808cdd7228f2b5a46e
taisuke <uwattotaitai@gmail.com>
2020-08-06 13:03:50 +0900

    initial commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    sku: string;
+    name: string;
+    description: string;
+    unitPlace: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+}
